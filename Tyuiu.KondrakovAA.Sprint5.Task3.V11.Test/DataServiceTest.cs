d8626b45c33cb1abd9581e224ddef5fc931e044c
KondrakovAlexander <144276548+KondrakovAlexander@users.noreply.github.com>
2023-11-19 22:32:43 +0500

    Task3

---
+++ Tyuiu.KondrakovAA.Sprint5.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KondrakovAA.Sprint5.Task3.V11.Lib;
+
+namespace Tyuiu.KondrakovAA.Sprint5.Task3.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService resp = new DataService();
+            Assert.AreEqual(-2.556, resp.SaveToFileTextData(3));
+        }
+    }
+}
